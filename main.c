93f50a3656a347cff8dba69ae301c677ff751fc2
Jordan Justen <jljusten@gmail.com>
2017-04-18 23:23:57 -0700

    main: Only enable OpenGL when needed
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

--- main.c
+++ main.c
@@ -62,40 +62,63 @@ bool sdl_start()
 
     drawer->set_window_attributes();
 
+    bool is_gl = false;
+    switch (drawer->api_type) {
+    case API_NOT_SET:
+        assert(drawer->api_type != API_NOT_SET);
+        break;
+    case API_OPENGL_COMPAT:
+    case API_OPENGL_CORE:
+    case API_OPENGL_ES2:
+        is_gl = true;
+        break;
+    }
+
+    Uint32 window_create_flags = SDL_WINDOW_RESIZABLE;
+    /* window_create_flags |= SDL_WINDOW_FULLSCREEN_DESKTOP; */
+
+    if (is_gl)
+        window_create_flags |= SDL_WINDOW_OPENGL;
+
     window = SDL_CreateWindow("gears3d", SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              300, 300,
-                              SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE|
-                              (0?SDL_WINDOW_FULLSCREEN_DESKTOP:0));
+                              SDL_WINDOWPOS_UNDEFINED, 300, 300,
+                              window_create_flags);
 
-    context = SDL_GL_CreateContext(window);
-    if (!context) {
-        fprintf(stderr, "SDL_GL_CreateContext(): %s\n", SDL_GetError());
-        return false;
+    if (is_gl) {
+        context = SDL_GL_CreateContext(window);
+        if (!context) {
+            fprintf(stderr, "SDL_GL_CreateContext(): %s\n", SDL_GetError());
+            return false;
+        }
+
+        int w, h;
+        SDL_GL_MakeCurrent(window, context);
+        SDL_GL_GetDrawableSize(window, &w, &h);
+        glViewport(0, 0, w, h);
+        width = w;
+        height = h;
+
+        version = glGetString(GL_VERSION);
+        assert(version);
+        printf("GL Version: %s\n", version);
+
+        if (gears_options.vsync) {
+            SDL_GL_SetSwapInterval(1);
+        } else if (SDL_GL_SetSwapInterval(-1 /* late swap tearing */) == -1) {
+            /* late swap setup failed */
+            SDL_GL_SetSwapInterval(0 /* immediate updates */);
+        }
+
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+    } else {
+        int w, h;
+        SDL_GetWindowSize(window, &w, &h);
+        width = w;
+        height = h;
     }
-
-    int w, h;
-    SDL_GL_MakeCurrent(window, context);
-    SDL_GL_GetDrawableSize(window, &w, &h);
-    glViewport(0, 0, w, h);
-    width = w;
-    height = h;
-
-    version = glGetString(GL_VERSION);
-    assert(version);
-    printf("GL Version: %s\n", version);
-
-    if (gears_options.vsync) {
-        SDL_GL_SetSwapInterval(1);
-    } else if (SDL_GL_SetSwapInterval(-1 /* late swap tearing */) == -1) {
-        /* late swap setup failed */
-        SDL_GL_SetSwapInterval(0 /* immediate updates */);
-    }
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
 
     return true;
 }
