gears3d/gears3d

commit 11cdcffeb3bef295eba6ef0b6f3db2d5b31feda0

vk10: Be a bit more flexible with number of images
The WSI may want to return more or less than NUM_IMAGES, so save the
number into num_images. We still cap it at NUM_IMAGES due to some
other assumptions, but we then have to ignore some images returned
from vkAcquireNextImageKHR if the index is too large.

Signed-off-by: Jordan Justen <jljusten@gmail.com>

vk10.c
@@ -202,6 +202,7 @@ static const VkCommandPoolCreateInfo cmd_pool_create_info = {
 
 #define GEARS 3
 #define NUM_IMAGES 2
+uint32_t num_images = 0;
 
 static VkInstance instance;
 
@@ -1226,7 +1227,7 @@ create_wsi_images(int width, int height, VkImage *images)
         .pNext = 0,
         .flags = 0,
         .surface = surface,
-        .minImageCount = NUM_IMAGES,
+        .minImageCount = 1,
         .imageFormat = VK_FORMAT_B8G8R8A8_UNORM,
         .imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
         .imageExtent = { width, height },
@@ -1247,14 +1248,16 @@ create_wsi_images(int width, int height, VkImage *images)
                                     &swapchain);
     assert(res == VK_SUCCESS);
 
-    uint32_t image_count;
-    res = VFN(vkGetSwapchainImagesKHR)(device, swapchain, &image_count, NULL);
-    assert(res == VK_SUCCESS && image_count >= NUM_IMAGES);
-
-    image_count = NUM_IMAGES;
-    res = VFN(vkGetSwapchainImagesKHR)(device, swapchain, &image_count,
-                                       images);
+    uint32_t wsi_image_count;
+    res = VFN(vkGetSwapchainImagesKHR)(device, swapchain, &wsi_image_count,
+                                       NULL);
     assert(res == VK_SUCCESS);
+
+    num_images = NUM_IMAGES;
+    res = VFN(vkGetSwapchainImagesKHR)(device, swapchain, &num_images,
+                                       images);
+    assert(res == VK_SUCCESS ||
+           (res == VK_INCOMPLETE && num_images < wsi_image_count));
 }
 
 static void
@@ -1666,7 +1669,7 @@ get_wsi_image()
             if (timeouts > 20)
                 break;
         }
-    } while (res == VK_TIMEOUT);
+    } while (res == VK_TIMEOUT || index >= num_images);
 
     /* The window was probably resized, so skip this frame */
     if (res == VK_ERROR_OUT_OF_DATE_KHR || res == VK_SUBOPTIMAL_KHR) {

commit 435d5ed698c149c4929ef10b6f88cef8233fd6e9

vk10: Set num_images in create_non_wsi_images
Fixes: 11cdcff ("vk10: Be a bit more flexible with number of images")
Signed-off-by: Jordan Justen <jljusten@gmail.com>

vk10.c
@@ -1278,6 +1278,8 @@ create_non_wsi_images(int width, int height, VkImage *images)
         .initialLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
     };
 
+    num_images = NUM_IMAGES;
+
     int image_num;
     for (image_num = 0; image_num < NUM_IMAGES; image_num++) {
         res = VFN(vkCreateImage)(device, &info, NULL, &images[image_num]);