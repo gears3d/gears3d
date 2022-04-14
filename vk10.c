gears3d/gears3d

commit 11cdcffeb3bef295eba6ef0b6f3db2d5b31feda0

vk10: Be a bit more flexible with number of images
The WSI may want to return more or less than NUM_IMAGES, so save the
number into num_images. We still cap it at NUM_IMAGES due to some
other assumptions, but we then have to ignore some images returned
from vkAcquireNextImageKHR if the index is too large.

Signed-off-by: Jordan Justen <jljusten@gmail.com>

vk10.c
@@ -202,6 +202,7 @@ static const VkCommandPoolCreateInfo cmd_pool_create_info = {
 
 #define GEARS 3
 #define NUM_IMAGES 2
+uint32_t num_images = 0;
 
 static VkInstance instance;
 
@@ -1226,7 +1227,7 @@ create_wsi_images(int width, int height, VkImage *images)
         .pNext = 0,
         .flags = 0,
         .surface = surface,
-        .minImageCount = NUM_IMAGES,
+        .minImageCount = 1,
         .imageFormat = VK_FORMAT_B8G8R8A8_UNORM,
         .imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
         .imageExtent = { width, height },
@@ -1247,14 +1248,16 @@ create_wsi_images(int width, int height, VkImage *images)
                                     &swapchain);
     assert(res == VK_SUCCESS);
 
-    uint32_t image_count;
-    res = VFN(vkGetSwapchainImagesKHR)(device, swapchain, &image_count, NULL);
-    assert(res == VK_SUCCESS && image_count >= NUM_IMAGES);
-
-    image_count = NUM_IMAGES;
-    res = VFN(vkGetSwapchainImagesKHR)(device, swapchain, &image_count,
-                                       images);
+    uint32_t wsi_image_count;
+    res = VFN(vkGetSwapchainImagesKHR)(device, swapchain, &wsi_image_count,
+                                       NULL);
     assert(res == VK_SUCCESS);
+
+    num_images = NUM_IMAGES;
+    res = VFN(vkGetSwapchainImagesKHR)(device, swapchain, &num_images,
+                                       images);
+    assert(res == VK_SUCCESS ||
+           (res == VK_INCOMPLETE && num_images < wsi_image_count));
 }
 
 static void
@@ -1666,7 +1669,7 @@ get_wsi_image()
             if (timeouts > 20)
                 break;
         }
-    } while (res == VK_TIMEOUT);
+    } while (res == VK_TIMEOUT || index >= num_images);
 
     /* The window was probably resized, so skip this frame */
     if (res == VK_ERROR_OUT_OF_DATE_KHR || res == VK_SUBOPTIMAL_KHR) {

commit 21a30b39bc1a2782047429293d40b5f00e553429

vk10: Use num_images instead of NUM_IMAGES
Fixes: 11cdcff ("vk10: Be a bit more flexible with number of images")
Signed-off-by: Jordan Justen <jljusten@gmail.com>

vk10.c
@@ -1281,14 +1281,14 @@ create_non_wsi_images(int width, int height, VkImage *images)
     num_images = NUM_IMAGES;
 
     int image_num;
-    for (image_num = 0; image_num < NUM_IMAGES; image_num++) {
+    for (image_num = 0; image_num < num_images; image_num++) {
         res = VFN(vkCreateImage)(device, &info, NULL, &images[image_num]);
         assert(res == VK_SUCCESS);
         non_wsi_images[image_num] = images[image_num];
     }
 
     VkMemoryRequirements mem_req[NUM_IMAGES];
-    for (image_num = 0; image_num < NUM_IMAGES; image_num++) {
+    for (image_num = 0; image_num < num_images; image_num++) {
         VFN(vkGetImageMemoryRequirements)(device, images[image_num],
                                           &mem_req[image_num]);
     }
@@ -1297,7 +1297,7 @@ create_non_wsi_images(int width, int height, VkImage *images)
         find_memory_idx(mem_req[0].memoryTypeBits,
                         VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 
-    for (image_num = 1; image_num < NUM_IMAGES; image_num++) {
+    for (image_num = 1; image_num < num_images; image_num++) {
         assert(find_memory_idx(mem_req[image_num].memoryTypeBits,
                                VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT) ==
                mem_ty_idx);
@@ -1306,7 +1306,7 @@ create_non_wsi_images(int width, int height, VkImage *images)
     VkDeviceSize image_size = 0;
     uint32_t image_offsets[NUM_IMAGES];
     image_offsets[0] = 0;
-    for (image_num = 1; image_num < NUM_IMAGES; image_num++) {
+    for (image_num = 1; image_num < num_images; image_num++) {
         image_size = ALIGN(image_size, mem_req[image_num].alignment);
         image_offsets[image_num] = image_size;
         image_size += mem_req[image_num].size;
@@ -1320,7 +1320,7 @@ create_non_wsi_images(int width, int height, VkImage *images)
     res = VFN(vkAllocateMemory)(device, &alloc_info, NULL, &non_wsi_mem);
     assert(res == VK_SUCCESS);
 
-    for (image_num = 0; image_num < NUM_IMAGES; image_num++) {
+    for (image_num = 0; image_num < num_images; image_num++) {
         res = VFN(vkBindImageMemory)(device, images[image_num], non_wsi_mem,
                                      image_offsets[image_num]);
         assert(res == VK_SUCCESS);
@@ -1449,7 +1449,7 @@ create_pipeline(int width, int height)
     };
 
     int image_num;
-    for (image_num = 0; image_num < NUM_IMAGES; image_num++) {
+    for (image_num = 0; image_num < num_images; image_num++) {
 
         VkImageViewCreateInfo image_view_create_info = {
             .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
@@ -1716,7 +1716,7 @@ static int
 submitted_pos()
 {
     return
-        (next_render_pos + NUM_IMAGES - submitted_render_count) % NUM_IMAGES;
+        (next_render_pos + num_images - submitted_render_count) % num_images;
 }
 
 static void
@@ -1756,7 +1756,7 @@ check_for_complete_frames()
         post_draw();
 
         submitted_render_count--;
-        pos = (pos + 1) % NUM_IMAGES;
+        pos = (pos + 1) % num_images;
     }
 }
 
@@ -1776,7 +1776,7 @@ draw()
 
     pthread_mutex_lock(&win_size_lock);
 
-    if (submitted_render_count == NUM_IMAGES)
+    if (submitted_render_count == num_images)
         flush_one_render();
 
     if (submitted_render_count > 0)
@@ -1806,7 +1806,7 @@ draw()
 
     submitted_render_count++;
     in_flight_indices[next_render_pos] = index;
-    next_render_pos = (next_render_pos + 1) % NUM_IMAGES;
+    next_render_pos = (next_render_pos + 1) % num_images;
 
     pthread_mutex_unlock(&win_size_lock);
 }